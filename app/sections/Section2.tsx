c7caf92c7522e9e5968b110b28f4cc6b74c0bafa
Osotatanekeeni <tatanekari52@gmail.com>
2026-01-05 14:39:52 +0100

    feat:made section 2 mobile responsive

--- app/sections/Section2.tsx
+++ app/sections/Section2.tsx
@@ -3,21 +3,21 @@ import React from 'react'
 
 export default function Section2() {
   return (
-    <div className="w-full flex flex-col items-center h-[100vh] bg-[#F8F9FA]">
-        <div className='mt-20'>
-          <div className="">
+    <div className="w-full flex flex-col items-center min-h-screen py-12 md:py-16 lg:py-20 px-4 md:px-8 bg-[#F8F9FA]">
+        <div className='w-full max-w-6xl'>
+          <div className="mb-6 md:mb-8 lg:mb-10">
         <div>
-          <p className="font-[family-name:var(--font-noto-serif-georgian)] text-5xl text-center">
+          <p className="font-[family-name:var(--font-noto-serif-georgian)] text-2xl md:text-4xl lg:text-5xl text-center px-2">
             What If Your Wellness Had A Guide You Could,
           </p>
-          <p className="font-[family-name:var(--font-charm)] text-[#2D7967] text-5xl text-center font-bold mt-2">
+          <p className="font-[family-name:var(--font-charm)] text-[#2D7967] text-2xl md:text-4xl lg:text-5xl text-center font-bold mt-2">
             talk to anytime ?
           </p>
         </div>
       </div>
 
-      <div>
-        <Image src={"/images/section2Image.png"} alt="Section2 Image" width={1440} height={600} className="w-full" />
+      <div className="w-full">
+        <Image src={"/images/section2Image.png"} alt="Section2 Image" width={1440} height={600} className="w-full h-auto" />
       </div>
         </div>
     </div>
